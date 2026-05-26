e28841723416967c94cf7da33fe2ec7a71f8eb0e
Ranica10 <ranicachawla101@gmail.com>
2026-05-25 23:28:38 -0400

    Edit navbar to be sticky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,33 @@
-import React, {useState} from "react";
+import {useState} from "react";
 
 import styles from "./Navbar.module.css";
 import {getImageUrl} from "../../utils";
 
 export const Navbar = () => {
-    /* Close and open menu icon */
     const [menuOpen, setMenuOpen] = useState(false)
 
     return <nav className={styles.navbar}>
-        {/* Return back to home page */}
-        <a className = {styles.title} href="/">Portfolio</a>
-        <div className = {styles.menu}>
-            {/* Navbar menu icon */}
+        <a className={styles.title} href="/">
+            <span className={styles.logoMark}>RC</span>
+            <span>Portfolio</span>
+        </a>
+        <div className={styles.menu}>
             <img 
-                className = {styles.menuBtn} 
-                /* change image of menu icon based on state */
-                src = {
+                className={styles.menuBtn} 
+                src={
                     menuOpen 
                     ? getImageUrl("nav/closeIcon.png") 
                     : getImageUrl("nav/menuIcon.png") 
                 } 
-                alt = "menu-button"
-                /* set state to opposite of current */
+                alt="menu-button"
                 onClick={() => setMenuOpen(!menuOpen)}
             />
-            {/* List of all menu items */}
-            <ul className = {
-                    /* Apply this style only when menu open */
-                    `${styles.menuItems} 
-                    ${menuOpen && styles.menuOpen}`
-                }
-                /* Close the menu when item clicked */
-                onClick = {() => setMenuOpen(false)}
-                >
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#tech-stack">Tech Stack</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a className={styles.contactPill} href="#contact">Contact</a></li>
             </ul>
         </div>
     </nav>;
